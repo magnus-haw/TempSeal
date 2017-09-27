d67a9ab427d4f2945434c64d60f8c514247690bf
Magnus <magnus.haw@gmail.com>
2017-09-27 00:02:27 -0700

    updated timezone and admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -8,6 +8,11 @@ from django.contrib import admin
 from .models import Question,Choice,SingleResponse,Building
 admin.site.register(Choice)
 admin.site.register(Question)
-
-admin.site.register(SingleResponse)
 admin.site.register(Building)
+
+class SingleResponseAdmin(admin.ModelAdmin):
+    list_display =('timestamp','student_ID','building','room','temp')
+    list_filter = ['timestamp']
+    search_fields = ['building__name','student_ID']
+
+admin.site.register(SingleResponse,SingleResponseAdmin)
