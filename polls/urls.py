cfa48ecd4602bd06fd78555c91e66e3062227a4f
Magnus <magnus.haw@gmail.com>
2017-09-27 00:32:03 -0700

    updated url config to include admin interface

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import include,url
+from django.contrib import admin
+admin.autodiscover()
 
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
